fa391961553b9a84037a1bfbbf7f846dbd64642c
Ruoxi1028 <Ruoxi1028@users.noreply.github.com>
2026-08-10 19:13:04 +0800

    fix: 补全 WallpaperScreen 布局 import 修复编译

--- android/app/src/main/java/com/linxi/diary/ui/screens/WallpaperScreen.kt
+++ android/app/src/main/java/com/linxi/diary/ui/screens/WallpaperScreen.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
