8799b537b29f5dd84f63d4612694a82f3a6e9ca9
Ruoxi1028 <Ruoxi1028@users.noreply.github.com>
2026-08-11 10:30:10 +0800

    test(android): 更新 MainFabDestination 测试适配发现页无 FAB

--- android/app/src/test/java/com/linxi/diary/ui/navigation/MainFabDestinationTest.kt
+++ android/app/src/test/java/com/linxi/diary/ui/navigation/MainFabDestinationTest.kt
@@ -5,10 +5,10 @@ import org.junit.Test
 
 class MainFabDestinationTest {
     @Test
-    fun `只有待办和日记页面显示主层 FAB`() {
+    fun `只有待办页面显示主层 FAB`() {
         assertEquals(MainFabDestination.None, MainFabDestination.forPage(0))
         assertEquals(MainFabDestination.Todo, MainFabDestination.forPage(1))
-        assertEquals(MainFabDestination.Diary, MainFabDestination.forPage(2))
+        assertEquals(MainFabDestination.None, MainFabDestination.forPage(2))
         assertEquals(MainFabDestination.None, MainFabDestination.forPage(3))
     }
 }
